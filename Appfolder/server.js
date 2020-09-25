66ece4f14e61804bd13555af823b692c8aab92b1
Alex McDonald <acm4219@gmail.com>
2020-09-24 21:42:20 -0500

    attempted to add express again and updated the server.js

--- Appfolder/server.js
+++ Appfolder/server.js
@@ -1,4 +1,5 @@
 const { json } = require("express");
+const { json } = require("fs");
 // Dependencies
 // =============================================================
 var express = require("express");
@@ -12,11 +13,12 @@ var PORT = process.env.PORT || 3000; //dynamic port
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/", function (req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
