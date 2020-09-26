582353f51d77b913606c51319c8ef342744f5150
Alex McDonald <acm4219@gmail.com>
2020-09-26 17:35:52 -0500

    got the server running a little bit still need to sort out the api portion

--- Appfolder/server.js
+++ Appfolder/server.js
@@ -12,8 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let notes = [];
-
+// html path
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -22,13 +21,42 @@ app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res) {});
+//Api path
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-app.post("/api/notes", function (req, res) {
-  console.log("request.body", req.body);
-  let newNotes = req.body;
-  notes.push(newNotes);
-});
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(data);
+  });
+
+  app.get("/api/notes", function (req, res) {
+    res.json(data[Number(req.params.id)]);
+  });
+
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let uniqueId = data.length.toString();
+    console.log(uniqueId);
+    newNote.id = uniqueId;
+    data.push(newNote);
+
+    res.json(data);
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    let noteId = req.params.id;
+    let newId = 0;
+    console.log(`Deleting note with id $(noteId)`);
+    data = data.filter((currentNote) => {
+      return currentNote.id != noteId;
+    });
+    for (currentNote of data) {
+      currentNote.id = newId.toString();
+      newId++;
+    }
+    res.json(data);
+  });
+};
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
