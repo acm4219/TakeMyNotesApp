41be4c7d4568b788942b0aee66d768e1bd81d447
Alex McDonald <acm4219@gmail.com>
2020-09-25 20:01:31 -0500

    finally got server working

--- Appfolder/public/server.js
+++ Appfolder/public/server.js
@@ -1,9 +1,8 @@
 const { json } = require("express");
-const { json } = require("fs");
+
 // Dependencies
 // =============================================================
 var express = require("express");
-var fs = require("fs");
 var path = require("path");
 
 // Sets up the Express App
@@ -16,10 +15,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/notes.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
