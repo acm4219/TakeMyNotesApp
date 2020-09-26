bccd4f1bc605a9092b47d6ec3622606f08e31ee7
Alex McDonald <acm4219@gmail.com>
2020-09-26 15:22:09 -0500

    FINALLY GOT THE MAIN STUFF WORKING

--- Appfolder/server.js
+++ Appfolder/server.js
@@ -8,17 +8,18 @@ var app = express();
 var PORT = process.env.PORT || 3000; //dynamic port
 
 // Sets up the Express app to handle data parsing
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let notes = [];
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {});
